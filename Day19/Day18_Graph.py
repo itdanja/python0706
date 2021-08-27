f83fa64c7c3364eb4774df3efd551e5fac64ed8d
Sera Lee <issell@naver.com>
2021-08-27 20:47:23 +0900

    day19

--- Day19/Day18_Graph.py
+++ Day19/Day18_Graph.py
@@ -10,7 +10,7 @@ class Graph() :
 nameAry = ["춘천" , "서울" , "속초" , "대전" , "광주" , "부산" ]
 춘천 = 0 ; 서울 = 1 ; 속초 = 2 ; 대전 = 3 ; 광주 = 4 ; 부산 = 5
 
-# 그래프 정점 찍는 함수
+# 콘솔에 그래프 정점 찍는 함수
 def printGraph(g) :
     print("   ", end=" ")
     for v in range( g.SIZE ) :
@@ -66,5 +66,79 @@ G1.graph[부산][대전] = 30; G1.graph[부산][광주] = 25
 
 print("## 자전거 도로 건설을 위한 전체 연결도 ##")
 printGraph(G1)
+
+# 가중치 간선 목록
+edgeAry = [ ]
+for i in range(gSize) :
+    for k in range(gSize) :
+        if G1.graph[i][k] != 0 :
+            edgeAry.append( [G1.graph[i][k] , i , k ] )
+                                # 비용  , 출발 , 도착
+from operator import itemgetter
+edgeAry = sorted( edgeAry , key=itemgetter(0) , reverse=True ) # 비용기준으로 정렬
+    # sorted( 리스트 ) : 오름차순
+    # sorted( 리스트 , reverse = True ) : 내림차순
+    # sorted( 2차원 리스트 , key=itemgetter(정렬기준의인덱스) , reverse = True )
+# edgeAry2 = sorted( edgeAry , key=itemgetter(1) , reverse=True )
+#print( edgeAry  )
+# print( edgeAry2  )
+newAry = [ ]
+for i in range( 0 , len(edgeAry) , 2 ) :  # 정렬된 리스트 저장 [ 출발-도착 // 도착-출발 => 동일하기 때문에 하나만 저장 ]
+    newAry.append( edgeAry[i] )
+# print( len(newAry) )
+index = 0
+while len(newAry) > gSize-1 : # 간선 개수가 정점 개수-1 일때까지 반복 [ 최소 비용 ]
+    start = newAry[index][1]    # 출발 간선
+    end = newAry[index][2]      # 도착 간선
+    saveCost = newAry[index][0] # 간선 비용 [ 내림차순 했기 때문에 앞 비용이 크다 ]
+
+    G1.graph[start][end] = 0
+    G1.graph[end][start] = 0
+
+    startYN = findVertex( G1 , start )
+
+    endYN = findVertex( G1 , end )
+
+    if startYN and endYN :   # 출발과 도착 1번
+        del( newAry[index] )
+    else:
+        G1.graph[start][end] = saveCost
+        G1.graph[end][start] = saveCost
+        index += 1
+print("## 최소 비용의 자전거 도로 연결도 ## ")
+printGraph(G1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
